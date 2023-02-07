da5ca3093609cb90e39317d94d6d87a6566840e3
Abiodun <abiodunhodonu@gmail.com>
2023-02-07 22:39:09 +0100

    todo tweaking

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 import {uid} from 'uid';
+import { Icon } from '@iconify/vue';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
 const todoList = ref([]);
 
 const createTodo = (todo) => {
@@ -19,6 +21,13 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="openmoji:sad-but-relieved-face" color="#41b080" width="22" />
+      <span>You have no todo's to complete. Add ASAP!</span>
+    </p>
   </main>
 </template>
 
@@ -33,6 +42,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
